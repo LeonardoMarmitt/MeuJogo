dd682f7b3e09870ba4f5a72c67c3cf9db4449578
LeonardoMarmitt <leonardomarmitt@hotmail.com>
2025-06-27 22:42:12 +0800

    StarUML Classes

---
+++ code/animal.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+class animal:
+    def __init__(self):
+        self.age = None
+        self.height = None
+        self.wieght = None
+        self.position = None
+
+    def move_x(self, ):
+        pass
+
+    def move_y(self, ):
+        pass
+
+    def move_z(self, ):
+        pass
